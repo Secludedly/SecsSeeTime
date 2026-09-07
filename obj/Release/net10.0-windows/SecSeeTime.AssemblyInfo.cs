cddcee0f9782420140d7005eac175f886a2ee554
Secludedly <GogoplataSteve@Gmail.com>
2026-09-06 21:47:28 -0400

    gitignore

--- obj/Release/net10.0-windows/SecSeeTime.AssemblyInfo.cs
+++ obj/Release/net10.0-windows/SecSeeTime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecSeeTime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f81563bc88cc1ca92b290dd5a0e7d3106af0760")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecSeeTime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecSeeTime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
